8f4eb728ef99ff2bd94e2e71ba6058381e35607f
Breno Leite <brenoleitepereira@gmail.com>
2023-04-25 20:23:47 -0300

    Fix: Correção de elementos errados conforme figma

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,30 @@
 import Link from "next/link";
-import "./header.css"
-import Logo from "../../assets/logo.png"
+import "./header.css";
+import Logo from "../../assets/logo.png";
 import Image from "next/image";
-
 
 const Header = () => {
   return (
     <header className="header">
-      
       <Link href="/">
-        <Image src={Logo} alt="Logo" className="header_image"/>
-      
+        <Image src={Logo} alt="Logo" className="header_image" />
       </Link>
 
-        <nav>
-            <ul>
-                <li>
-                  <Link href="/movies">FILMES</Link> 
-                  </li>
-                <li>
-                <Link href="/series">SÉRIES</Link> 
-                </li>
-                <li>
-                <Link href="/tops">TOP10</Link> 
-                </li>
-            </ul>
-        </nav>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/movies">FILMES</Link>
+          </li>
+          <li>
+            <Link href="/series">SÉRIES</Link>
+          </li>
+          <li>
+            <Link href="/tops">TOP10</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
